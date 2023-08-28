2c62efdec3a2544f5b2c57a67ee8681d88684c16
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2023-08-27 23:29:35 -0300

    Local Storage para o theme

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const data = {
+  name: "Luis Gustavo",
+  description:
+    "Sou apaixonado por tecnologias! As pessoas que me inspiram são aquelas que compartilham da mesma paixão",
+}
+
+const links = {
+  linkedin: "https://www.linkedin.com/in/luis-gusta-oliveira/",
+  youtube: "https://www.youtube.com/channel/UC_zSzQfSnbqxRqLys6sAv8g",
+  twitch: "https://www.twitch.tv/lgustta13",
+  instagram: "https://www.instagram.com/luis_gustta13/",
+  github: "https://github.com/LGustta13",
+}
+
+const projetos_texto = {
+  item1: "IoT e Telemetria",
+  item2: "Artigo - Cloud Computing",
+  item3: "NextJS - Time Traveller",
+  item4: "Certificações",
+}
+
+const projetos_links = {
+  link1:
+    "https://drive.google.com/file/d/1FqV_F_vm_5pQEgPEcDSI5t4S6yIWn7tD/view",
+  link2:
+    "https://web.dio.me/articles/internet-das-coisas-e-seu-espaco-na-computacao-em-nuvem?back=%2Farticles&page=1&order=oldes",
+  link3: "https://traveller-lgusta.vercel.app/",
+  link4: "#",
+}
+
+document.getElementById("name").innerHTML = data.name
+document.getElementById("description").innerHTML = data.description
--- js/toogleMode.js
+++ js/toogleMode.js
@@ -1,9 +1,20 @@
+if(localStorage.getItem("light-theme") == "true"){
+  document.documentElement.classList.toggle("light")
+}
+
 function toogleMode() {
 
   // Alterar a classe light no HTML
   const html = document.documentElement; // root
-  html.classList.toggle('light');
+  let value = html.classList.toggle('light');
 
+  if(value){
+    localStorage.setItem("light-theme", true)
+  } else {
+    localStorage.setItem("light-theme", false)
+  }
+  
+  
   // Alterar a imagem do usuário pelo CSS
   const img = document.querySelector('#profile img') // #profile img
   
@@ -12,4 +23,6 @@ function toogleMode() {
   } else {
     img.setAttribute("src", "./assets/avatar-gusta.jpg")
   }
+
+  return value;
 }
